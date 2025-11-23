b5b94b246abf474cf0e4cc689d2ef8e60a37d0fd
Carlos Estrague  / Mrc_munir <maximu@gmail.com>
2025-11-23 17:39:24 +0100

    wip

--- .ci/build-linux-aarch64.sh
+++ .ci/build-linux-aarch64.sh
@@ -1,55 +1,46 @@
 #!/bin/sh -ex
 
-cd rpcs3 || exit 1
+git config --global --add safe.directory '*'
+
+# Pull all the submodules except llvm and opencv
+# shellcheck disable=SC2046
+git submodule -q update --init $(awk '/path/ && !/llvm/ && !/opencv/ { print $3 }' .gitmodules)
+
+if [ "$COMPILER" = "gcc" ]; then
+    export CC=gcc-14
+    export CXX=g++-14
+else
+    export CC=clang
+    export CXX=clang++
+    export CFLAGS="$CFLAGS -fuse-ld=lld"
+fi
+
+cmake -B build                                         \
+    -DCMAKE_INSTALL_PREFIX=/usr                        \
+    -DCMAKE_C_FLAGS="$CFLAGS"                          \
+    -DCMAKE_CXX_FLAGS="$CFLAGS"                        \
+    -DUSE_NATIVE_INSTRUCTIONS=OFF                      \
+    -DUSE_PRECOMPILED_HEADERS=OFF                      \
+    -DUSE_SYSTEM_CURL=ON                               \
+    -DUSE_SDL=OFF                                      \
+    -DUSE_SYSTEM_FFMPEG=OFF                            \
+    -DUSE_SYSTEM_CURL=OFF                              \
+    -DUSE_SYSTEM_OPENAL=OFF                            \
+    -DUSE_DISCORD_RPC=ON                               \
+    -DOpenGL_GL_PREFERENCE=LEGACY                      \
+    -DSTATIC_LINK_LLVM=ON                              \
+    -DBUILD_LLVM=on                                    \
+    -G Ninja
+
+cmake --build build; build_status=$?;
 
 shellcheck .ci/*.sh
 
-git config --global --add safe.directory '*'
-
-# Pull all the submodules except some
-# shellcheck disable=SC2046
-git submodule -q update --init $(awk '/path/ && !/llvm/ && !/opencv/ && !/libsdl-org/ && !/curl/ && !/zlib/ { print $3 }' .gitmodules)
-
-mkdir build && cd build || exit 1
-
-if [ "$COMPILER" = "gcc" ]; then
-    # These are set in the dockerfile
-    export CC="${GCC_BINARY}"
-    export CXX="${GXX_BINARY}"
-    export LINKER=gold
-else
-    export CC="${CLANG_BINARY}"
-    export CXX="${CLANGXX_BINARY}"
-    export LINKER="${LLD_BINARY}"
-fi
-
-export LINKER_FLAG="-fuse-ld=${LINKER}"
-
-cmake ..                                               \
-    -DCMAKE_INSTALL_PREFIX=/usr                        \
-    -DUSE_NATIVE_INSTRUCTIONS=OFF                      \
-    -DUSE_PRECOMPILED_HEADERS=OFF                      \
-    -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAG}"          \
-    -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAG}"       \
-    -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAG}"       \
-    -DUSE_SYSTEM_CURL=ON                               \
-    -DUSE_SDL=ON                                       \
-    -DUSE_SYSTEM_SDL=ON                                \
-    -DUSE_SYSTEM_FFMPEG=OFF                            \
-    -DUSE_SYSTEM_OPENCV=ON                             \
-    -DUSE_DISCORD_RPC=ON                               \
-    -DOpenGL_GL_PREFERENCE=LEGACY                      \
-    -DLLVM_DIR=/opt/llvm/lib/cmake/llvm                \
-    -DSTATIC_LINK_LLVM=ON                              \
-    -DBUILD_RPCS3_TESTS="${RUN_UNIT_TESTS}"            \
-    -DRUN_RPCS3_TESTS="${RUN_UNIT_TESTS}"              \
-    -G Ninja
-
-ninja; build_status=$?;
-
-cd ..
-
 # If it compiled succesfully let's deploy.
-if [ "$build_status" -eq 0 ]; then
+# Azure and Cirrus publish PRs as artifacts only.
+{   [ "$CI_HAS_ARTIFACTS" = "true" ];
+} && SHOULD_DEPLOY="true" || SHOULD_DEPLOY="false"
+
+if [ "$build_status" -eq 0 ] && [ "$SHOULD_DEPLOY" = "true" ]; then
     .ci/deploy-linux.sh "aarch64"
 fi
