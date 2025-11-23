d187e6b06661a4c8055ef34a4fa21210480f69a8
Carlos Estrague  / Mrc_munir <maximu@gmail.com>
2025-11-23 17:41:47 +0100

    wip

--- .ci/build-linux.sh
+++ .ci/build-linux.sh
@@ -1,66 +1,48 @@
 #!/bin/sh -ex
 
-cd rpcs3 || exit 1
-
-shellcheck .ci/*.sh
-
 git config --global --add safe.directory '*'
 
-# Pull all the submodules except some
+# Pull all the submodules except llvm and opencv
 # Note: Tried to use git submodule status, but it takes over 20 seconds
 # shellcheck disable=SC2046
-git submodule -q update --init $(awk '/path/ && !/llvm/ && !/opencv/ && !/libsdl-org/ && !/curl/ && !/zlib/ { print $3 }' .gitmodules)
-
-mkdir build && cd build || exit 1
+git submodule -q update --init $(awk '/path/ && !/llvm/ && !/opencv/ { print $3 }' .gitmodules)
 
 if [ "$COMPILER" = "gcc" ]; then
     # These are set in the dockerfile
-    export CC="${GCC_BINARY}"
-    export CXX="${GXX_BINARY}"
-    export LINKER=gold
-    # We need to set the following variables for LTO to link properly
-    export AR=/usr/bin/gcc-ar-"$GCCVER"
-    export RANLIB=/usr/bin/gcc-ranlib-"$GCCVER"
-    export CFLAGS="-fuse-linker-plugin"
+    export CC=gcc-14
+    export CXX=g++-14
 else
-    export CC="${CLANG_BINARY}"
-    export CXX="${CLANGXX_BINARY}"
-    export LINKER=lld
-    export AR=/usr/bin/llvm-ar-"$LLVMVER"
-    export RANLIB=/usr/bin/llvm-ranlib-"$LLVMVER"
+    export CC=clang
+    export CXX=clang++
+    export LD=clang
+    export CFLAGS="$CFLAGS -fuse-ld=lld"
 fi
 
-export LINKER_FLAG="-fuse-ld=${LINKER}"
-
-cmake ..                                               \
+cmake -B build                                         \
     -DCMAKE_INSTALL_PREFIX=/usr                        \
-    -DUSE_NATIVE_INSTRUCTIONS=OFF                      \
-    -DUSE_PRECOMPILED_HEADERS=OFF                      \
     -DCMAKE_C_FLAGS="$CFLAGS"                          \
     -DCMAKE_CXX_FLAGS="$CFLAGS"                        \
-    -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAG}"          \
-    -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAG}"       \
-    -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAG}"       \
-    -DCMAKE_AR="$AR"                                   \
-    -DCMAKE_RANLIB="$RANLIB"                           \
-    -DUSE_SYSTEM_CURL=ON                               \
-    -DUSE_SDL=ON                                       \
-    -DUSE_SYSTEM_SDL=ON                                \
+    -DUSE_NATIVE_INSTRUCTIONS=OFF                      \
+    -DUSE_PRECOMPILED_HEADERS=OFF                      \
+    -DUSE_SDL=OFF                                      \
+    -DUSE_SYSTEM_CURL=OFF                              \
+    -DUSE_SYSTEM_OPENAL=OFF                            \
     -DUSE_SYSTEM_FFMPEG=OFF                            \
-    -DUSE_SYSTEM_OPENCV=ON                             \
     -DUSE_DISCORD_RPC=ON                               \
     -DOpenGL_GL_PREFERENCE=LEGACY                      \
-    -DLLVM_DIR=/opt/llvm/lib/cmake/llvm                \
     -DSTATIC_LINK_LLVM=ON                              \
-    -DBUILD_RPCS3_TESTS="${RUN_UNIT_TESTS}"            \
-    -DRUN_RPCS3_TESTS="${RUN_UNIT_TESTS}"              \
+    -DBUILD_LLVM=on                                    \
     -G Ninja
 
-ninja; build_status=$?;
+cmake --build build; build_status=$?;
 
-cd ..
+shellcheck .ci/*.sh
 
 # If it compiled succesfully let's deploy.
-if [ "$build_status" -eq 0 ]; then
+# Azure and Cirrus publish PRs as artifacts only.
+{   [ "$CI_HAS_ARTIFACTS" = "true" ];
+} && SHOULD_DEPLOY="true" || SHOULD_DEPLOY="false"
+
+if [ "$build_status" -eq 0 ] && [ "$SHOULD_DEPLOY" = "true" ]; then
     .ci/deploy-linux.sh "x86_64"
 fi
